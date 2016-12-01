2616dbc9e4c5ff0a1ec8060a79b7a6155b42e476
Karthik Varma <leovarmak@hotmail.com>
2016-12-01 11:25:07 +0530

    Major BackEnd upgrades
    1/12/16 11:24AM

---
+++ js/monthhandler.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+    var d = new Date();
+    var n = d.getMonth();
+
+    if (n == 0){
+        setTimeout(function(){
+            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/jan.htm";
+        }, 8050);
+    } else if (n == 1) {
+      setTimeout(function(){
+            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/feb.htm";
+        }, 8050);
+    } else if (n == 2) {
+      setTimeout(function(){
+            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/march.htm";
+        }, 8050);
+    } else if (n == 3) {
+      setTimeout(function(){
+            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/april.htm";
+        }, 8050);
+    } else if (n == 4) {
+      setTimeout(function(){
+            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/may.htm";
+        }, 8050);
+    } else if (n == 5) {
+      setTimeout(function(){
+            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/june.htm";
+        }, 8050);
+    } else if (n == 6) {
+      setTimeout(function(){
+            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/july.htm";
+        }, 8050);
+    } else if (n == 7) {
+      setTimeout(function(){
+            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/aug.htm";
+        }, 8050);
+    } else if (n == 8) {
+      setTimeout(function(){
+            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/sep.htm";
+        }, 8050);
+    } else if (n == 9) {
+      setTimeout(function(){
+            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/oct.htm";
+        }, 8050);
+    } else if (n == 10) {
+      setTimeout(function(){
+            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/nov.htm";
+        }, 8050);
+    } else {
+      setTimeout(function(){
+            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/dec.htm";
+        }, 6600);
+    }
+
+})
