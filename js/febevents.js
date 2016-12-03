0b5f28ccf4c906bffde674aad7c093fc9383d86e
Karthik Varma <leovarmak@hotmail.com>
2016-12-03 23:09:00 +0530

    Added Events to Jan & Feb
    3/12/2016 11:08PM
    Added Events to Jan & Feb

---
+++ js/febevents.js
@@ -0,0 +1,113 @@
+$(document).ready(function(){
+      // MSNN SANTOSH BIRTHDAY
+        $("#1").click(function(){
+            swal({
+                title: "Mr. I. Daniel Selvaraj",
+                text: "🎂 Birthday",
+                imageUrl: "daniel.jpg",
+                // imageWidth: "128px",
+                // imageHeight: "128px",
+                // showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                // background: 'grey',
+                // cancelButtonColor: '#d33',
+                confirmButtonText: 'Next'
+              }).then(function () {
+                swal({
+                title: "Mrs. K. Satya",
+                text: "🎂 Birthday",
+                imageUrl: "satya.jpg",
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Close'
+              });
+              });
+      });
+        $("#2").click(function(){
+            swal({
+                title: "Mr. Sonal Ameet",
+                text: "🎂 Birthday",
+                imageUrl: "sonal.jpg",
+                // imageWidth: "128px",
+                // imageHeight: "128px",
+                // showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                // background: 'grey',
+                // cancelButtonColor: '#d33',
+                confirmButtonText: 'Close'
+              // }).then(function () {
+              //   swal({
+              //   title: "Harshita Meka",
+              //   text: "Birthday",
+              //   imageUrl: "harshita.jpg",
+              //   confirmButtonColor: '#3085d6',
+              //   confirmButtonText: 'Close'
+              // });
+              });
+      });
+        $("#3").click(function(){
+            swal({
+                title: "Mr. K.S. Yoganandh & Mrs. Djodilatchoumy",
+                text: "💍 Wedding Anniversary",
+                imageUrl: "noimage.jpg",
+                // imageWidth: "128px",
+                // imageHeight: "128px",
+                // showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                // background: 'grey',
+                // cancelButtonColor: '#d33',
+                confirmButtonText: 'Close'
+              // }).then(function () {
+              //   swal({
+              //   title: "Harshita Meka",
+              //   text: "Birthday",
+              //   imageUrl: "harshita.jpg",
+              //   confirmButtonColor: '#3085d6',
+              //   confirmButtonText: 'Close'
+              // });
+              });
+      });
+        $("#4").click(function(){
+            swal({
+                title: "Mr. T.N. Manoharan & Mrs.Sujatha",
+                text: "💍 Wedding Anniversary",
+                imageUrl: "manoharsujatha.jpg",
+                // imageWidth: "128px",
+                // imageHeight: "128px",
+                // showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                // background: 'grey',
+                // cancelButtonColor: '#d33',
+                confirmButtonText: 'Close'
+              // }).then(function () {
+              //   swal({
+              //   title: "Harshita Meka",
+              //   text: "Birthday",
+              //   imageUrl: "harshita.jpg",
+              //   confirmButtonColor: '#3085d6',
+              //   confirmButtonText: 'Close'
+              // });
+              });
+      });
+        $("#5").click(function(){
+            swal({
+                title: "Mr. K.S.B & Mrs. Satya",
+                text: "💍 Wedding Anniversary",
+                imageUrl: "ksbsatya.jpg",
+                // imageWidth: "128px",
+                // imageHeight: "128px",
+                // showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                // background: 'grey',
+                // cancelButtonColor: '#d33',
+                confirmButtonText: 'Close'
+              // }).then(function () {
+              //   swal({
+              //   title: "Harshita Meka",
+              //   text: "Birthday",
+              //   imageUrl: "harshita.jpg",
+              //   confirmButtonColor: '#3085d6',
+              //   confirmButtonText: 'Close'
+              // });
+              });
+      });
+  });
---
+++ js/janevents.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+      // MSNN SANTOSH BIRTHDAY
+        $("#1").click(function(){
+            swal({
+                title: "Mr. MSNN Santosh",
+                text: "🎂 Birthday",
+                imageUrl: "santosh.jpg",
+                // imageWidth: "128px",
+                // imageHeight: "128px",
+                // showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                // background: 'grey',
+                // cancelButtonColor: '#d33',
+                confirmButtonText: 'Close'
+              // }).then(function () {
+              //   swal({
+              //   title: "Harshita Meka",
+              //   text: "Birthday",
+              //   imageUrl: "harshita.jpg",
+              //   confirmButtonColor: '#3085d6',
+              //   confirmButtonText: 'Close'
+              // });
+              });
+      });
+        $("#2").click(function(){
+            swal({
+                title: "Mr. KS Yoganandh",
+                text: "🎂 Birthday",
+                imageUrl: "yoganandh.jpg",
+                // imageWidth: "128px",
+                // imageHeight: "128px",
+                // showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                // background: 'grey',
+                // cancelButtonColor: '#d33',
+                confirmButtonText: 'Close'
+              // }).then(function () {
+              //   swal({
+              //   title: "Harshita Meka",
+              //   text: "Birthday",
+              //   imageUrl: "harshita.jpg",
+              //   confirmButtonColor: '#3085d6',
+              //   confirmButtonText: 'Close'
+              // });
+              });
+      });
+        $("#3").click(function(){
+            swal({
+                title: "Mr. Ameet & Mrs. Sonal",
+                text: "💍 Wedding Anniversary",
+                imageUrl: "ameetsonal.jpg",
+                // imageWidth: "128px",
+                // imageHeight: "128px",
+                // showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                // background: 'grey',
+                // cancelButtonColor: '#d33',
+                confirmButtonText: 'Close'
+              // }).then(function () {
+              //   swal({
+              //   title: "Harshita Meka",
+              //   text: "Birthday",
+              //   imageUrl: "harshita.jpg",
+              //   confirmButtonColor: '#3085d6',
+              //   confirmButtonText: 'Close'
+              // });
+              });
+      });
+        $("#4").click(function(){
+            swal({
+                title: "Mr. G.R. Hari & Mrs. H. Anitha",
+                text: "💍 Wedding Anniversary",
+                imageUrl: "noimages.jpg",
+                // imageWidth: "128px",
+                // imageHeight: "128px",
+                // showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                // background: 'grey',
+                // cancelButtonColor: '#d33',
+                confirmButtonText: 'Close'
+              // }).then(function () {
+              //   swal({
+              //   title: "Harshita Meka",
+              //   text: "Birthday",
+              //   imageUrl: "harshita.jpg",
+              //   confirmButtonColor: '#3085d6',
+              //   confirmButtonText: 'Close'
+              // });
+              });
+      });
+  });
