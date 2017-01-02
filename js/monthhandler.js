5f3952e164d5e31c6aa3cd6e0d59521b8b6390fe
Karthik Varma <leovarmak@hotmail.com>
2017-01-02 17:13:08 +0530

    Updated Homepage

--- js/monthhandler.js
+++ js/monthhandler.js
@@ -3,54 +3,33 @@ $(document).ready(function(){
     var d = new Date();
     var n = d.getMonth();
 
+    setTimeout(function(){
+        
     if (n == 0){
-        setTimeout(function(){
             window.location = "https://leovarmak.github.io/MCA-Family-Calendar/jan.htm";
-        }, 8050);
     } else if (n == 1) {
-      setTimeout(function(){
             window.location = "https://leovarmak.github.io/MCA-Family-Calendar/feb.htm";
-        }, 8050);
-    } else if (n == 2) {
-      setTimeout(function(){
-            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/march.htm";
-        }, 8050);
+	  } else if (n == 2) {
+     		window.location = "https://leovarmak.github.io/MCA-Family-Calendar/march.htm";
     } else if (n == 3) {
-      setTimeout(function(){
-            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/april.htm";
-        }, 8050);
+      		window.location = "https://leovarmak.github.io/MCA-Family-Calendar/april.htm";
     } else if (n == 4) {
-      setTimeout(function(){
-            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/may.htm";
-        }, 8050);
+      		window.location = "https://leovarmak.github.io/MCA-Family-Calendar/may.htm";
     } else if (n == 5) {
-      setTimeout(function(){
-            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/june.htm";
-        }, 8050);
+      		window.location = "https://leovarmak.github.io/MCA-Family-Calendar/june.htm";
     } else if (n == 6) {
-      setTimeout(function(){
-            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/july.htm";
-        }, 8050);
+     		window.location = "https://leovarmak.github.io/MCA-Family-Calendar/july.htm";
     } else if (n == 7) {
-      setTimeout(function(){
-            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/aug.htm";
-        }, 8050);
+      		window.location = "https://leovarmak.github.io/MCA-Family-Calendar/aug.htm";
     } else if (n == 8) {
-      setTimeout(function(){
-            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/sep.htm";
-        }, 8050);
+      		window.location = "https://leovarmak.github.io/MCA-Family-Calendar/sep.htm";
     } else if (n == 9) {
-      setTimeout(function(){
-            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/oct.htm";
-        }, 8050);
+      		window.location = "https://leovarmak.github.io/MCA-Family-Calendar/oct.htm";
     } else if (n == 10) {
-      setTimeout(function(){
-            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/nov.htm";
-        }, 8050);
+      		window.location = "https://leovarmak.github.io/MCA-Family-Calendar/nov.htm";
     } else {
-      setTimeout(function(){
-            window.location = "https://leovarmak.github.io/MCA-Family-Calendar/dec.htm";
-        }, 6600);
+      		window.location = "https://leovarmak.github.io/MCA-Family-Calendar/dec.htm";
     }
+  }, 6000);
 
 })
